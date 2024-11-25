aaacd46cf905280da1db22dc3496a2939496d4b4
notDuck <daylenfrancis908@gmail.com>
2024-11-25 17:32:36 -0500

    fixing some deployment issues

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -6,7 +6,7 @@ function Intro() {
                     <h2 className="intro text-3xl max-md:text-xl ">Hello my name is Daylen.</h2>
                 </div>
                 <div className=" ">
-                    <p className="description text-white text-sm ">
+                    <p className="description text-white text-sm max-lg:w-11/12">
                         // fullstack developer with a focus in backend development in Wilmington, NC.</p>
                 </div>
             </section>
