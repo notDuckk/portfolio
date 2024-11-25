1f43ad6b8e2da8e12234afbecee63d258ff63876
notDuck <daylenfrancis908@gmail.com>
2024-11-25 16:19:20 -0500

    fixing some deployment issues

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -5,7 +5,7 @@ function Intro() {
                 <div className="inline-block ">
                     <h2 className="mt-[45vh] intro text-3xl max-md:text-xl ">Hello my name is Daylen.</h2>
                 </div>
-                <div className="mb-[40vh] flex justify-center container  max-xl:w-11/12 ">
+                <div className="mb-[40vh] flex justify-center justify-self-center container  max-xl:w-11/12 ">
                     <p className="description text-white text-sm ">
                         // fullstack developer with a focus in backend development in Wilmington, NC.</p>
                 </div>
