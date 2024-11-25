626534dafc82f580611c5340d56b9302daf290a7
notDuck <daylenfrancis908@gmail.com>
2024-11-25 17:43:15 -0500

    fixing some deployment issues

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,12 +1,12 @@
 function Intro() {
     return (
         <>
-            <section className="flex flex-col justify-center  items-center h-screen text-center">
+            <section className="flex flex-col justify-center  items-center h-screen text-center ">
                 <div>
                     <h2 className="intro text-3xl max-md:text-xl ">Hello my name is Daylen.</h2>
                 </div>
-                <div className=" ">
-                    <p className="description text-white text-sm max-lg:w-11/12">
+                <div className=" w-11/12">
+                    <p className="description text-white text-sm ">
                         // fullstack developer with a focus in backend development in Wilmington, NC.</p>
                 </div>
             </section>
